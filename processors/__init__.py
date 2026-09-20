d06963ab4c5f14dc25427f05b1048fd07c0fda21
Ronz333 <ronz.rw@gmail.com>
2026-09-20 13:00:26 +0200

    Refactor __init__.py to include processor imports

--- processors/__init__.py
+++ processors/__init__.py
@@ -1,1 +1,19 @@
-# Marker-Datei für das Processor-Plugin-Paket
+from .base_processor import BaseProcessor
+from .general_processor import GeneralDocumentProcessor
+from .pcb_eda_processor import PcbEdaProcessor
+from .python_processor import PythonProcessor
+from .java_processor import JavaProcessor
+from .javascript_processor import JavascriptProcessor
+from .oshw_circuit_processor import OshwCircuitProcessor
+from .processor_registry import registry
+
+__all__ = [
+    "BaseProcessor",
+    "GeneralDocumentProcessor",
+    "PcbEdaProcessor",
+    "PythonProcessor",
+    "JavaProcessor",
+    "JavascriptProcessor",
+    "OshwCircuitProcessor",
+    "registry"
+]
